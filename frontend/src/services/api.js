c550534ff1879f0da30b67d1ecf46f35a73fc450
NguyenTranAnhTho <ntho0124@gmail.com>
2025-10-30 23:08:30 +0700

    fix: env variable for API baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 // Cấu hình API với endpoint từ biến môi trường
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: `${process.env.REACT_APP_API_URL || "https://group13-project-backend.onrender.com"}/api`,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 
 // Nếu cần gửi token
 API.interceptors.request.use((config) => {
